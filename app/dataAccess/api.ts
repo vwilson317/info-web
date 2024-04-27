e0b65b9e302a3cd1181ef7014f0c0863e8852295
vincent <vincent.j.wilson317@gmail.com>
2024-04-27 20:54:41 +0100

    updated route for count

--- app/dataAccess/api.ts
+++ app/dataAccess/api.ts
@@ -1,7 +1,7 @@
 
 const Api = {
     async getCountAsync(): Promise<number> {
-        const response = await fetch('http://localhost:3001/api/count');
+        const response = await fetch('http://localhost:3001/api/v1/reviews/count');
         const data = await response.json();
     return data.count;
     }
