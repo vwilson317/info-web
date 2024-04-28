8d50db0a3dd8857cea7cd6ba90692a4aafd37029
vincent <vincent.j.wilson317@gmail.com>
2024-04-28 11:33:30 +0100

    moved api project to another repo. updates for env variable. using loader pattern

--- app/components/Counter.tsx
+++ app/components/Counter.tsx
@@ -1,22 +1,27 @@
 import { useEffect, useState } from 'react';
-import Api from '../dataAccess/api';
+// import Api from '../dataAccess/api';
+import { useLoaderData } from '@remix-run/react';
+import loader from '../routes/_index';
+
+// export async function loader(): Promise<number> {
+//     debugger
+//     console.log(process.env.API_URL);
+//     return Api.getCountAsync();
+// }
+
+// const loader = async () => {
+//         debugger
+//     const x = process.env.API_URL;
+//     return Api.getCountAsync();
+// }
 
 export default function Counter() {
     const [count, setCount] = useState(0);
-
-    const getCount = async () => {
-        const result = await Api.getCountAsync();
-        setCount(result);
-
-    }
-    useEffect(() => {
-        getCount();
-    }, []);
-
+    const data = useLoaderData<typeof loader>();
 
     return (
         <div>
-            <h1 style={style}>{count}</h1>
+            <h1 style={style}>{data}</h1>
         </div>
     );
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,10 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import { useEffect } from "react";
 import { createPortal } from "react-dom";
 import Counter from "~/components/Counter";
 import Wait from "~/components/Wait";
+import Api from '../dataAccess/api';
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,7 +13,13 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export function loader() {
+  return Api.getCountAsync();
+}
+
 export default function Index() {
+  // const z = useLoaderData<typeof loader>();
+  // debugger
   const x: any = (
     <div style={style}>
       <Counter />
